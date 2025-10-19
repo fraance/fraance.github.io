a3d71354bba1599beb63e27687cc2e49aceb16ca
Evrard-Nil Daillet <evrardnild@gmail.com>
2025-10-19 08:48:02 -0700

    feat: add favicon.ico and update metadata for improved site branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const playfair = Playfair_Display({
 export const metadata: Metadata = {
   title: siteMetadata.title,
   description: siteMetadata.description,
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
